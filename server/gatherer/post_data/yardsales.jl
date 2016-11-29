f0bad0f44ac461f75188dfaf65ea409dc0fd5257
Nathaniel Saul <nat@saulgill.com>
2016-11-28 19:05:45 -0800

    no more .jl files in repo

--- server/gatherer/post_data/yardsales.jl
+++
@@ -1,2 +0,0 @@
-{"url": "https://portland.craigslist.org/mlt/gms/5893901060.html", "title": ["Huge Multi-Estate **Indoor** Sale SE 72nd Ave."], "address": ["SE 72nd ave at SE Harold"]}
-{"url": "https://portland.craigslist.org/mlt/gms/5893921313.html", "title": ["HUGE INDOOR GARAGE SALE 10AM SUNDAY SE 72ND AVE-3 BLKS SOUTH OF FOSTER"], "address": ["SE INSLEY at SE 72ND"]}
