f113d2a506945bbedc4d4a5b353de29cd8e35d85
Nathaniel Saul <nat@saulgill.com>
2016-11-28 19:06:34 -0800

    Load, convert, save, and log

--- server/compass/ProcessSales.py
+++ server/compass/ProcessSales.py
@@ -4,17 +4,27 @@
 import Util
 import json
 
-# load the data
-# run each one through the thang
+# load the data as JSON lines
+# get lng/lat for each post's address
+# save as new JSON lines so compass can access
+
+infilename = 'gatherer/post_data/yardsales.jl'
+print("Loading json data from " + infilename)
 
 post_data = []
-with open('gatherer/post_data/yardsales.jl', 'r') as f:
+with open(infilename, 'r') as f:
     for line in f:
         post_data.append(json.loads(line))
 
+print("Finding address coordinates for %s address" % post_data.__len__())
 
 for post in post_data:
     post['coords'] = Util.get_coords_of_address(post['address'])
 
-import pdb; pdb.set_trace()
+outfilename = 'compass/sales/yardsales.jl'
+with open(outfilename, 'w') as f:
+    for post in post_data:
+        f.write(json.dumps(post)+"\n")
+
+print("Printing new json to " + outfilename)
 
